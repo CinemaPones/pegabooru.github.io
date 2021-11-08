d2b17e82510fb4b7b842a53c4414d4500c600a01
SimonPegasus <36254494+pegabooru@users.noreply.github.com>
2021-11-07 22:08:03 -0600

    Update handle.js

--- js/handle.js
+++ js/handle.js
@@ -53,7 +53,7 @@ file.addEventListener("change", ev => {
             url.innerText = data.data.link
             imglink = data.data.link
             document.getElementById("dirlink").value = data.data.link
-        } else if (document.getElementById("dirlink").value.trim() != "") {
+        } else if (document.getElementById("dirlink").value.length != 0) {
             img.src = document.getElementById("dirlink").value
             url.innerText = document.getElementById("dirlink").value
             imglink = document.getElementById("dirlink").value
@@ -118,7 +118,7 @@ function update() { // Update Google Sheets
         valueInputOption: 'RAW',
     };
     
-    if (document.getElementById("dirlink").value.trim() != "") {
+    if (document.getElementById("dirlink").value.length != 0) {
         img.src = document.getElementById("dirlink").value
         url.innerText = document.getElementById("dirlink").value
         imglink = document.getElementById("dirlink").value
