e36a7827d063af840856cb6c1edf44158bdfd689
SimonPegasus <36254494+pegabooru@users.noreply.github.com>
2021-11-12 11:05:46 -0600

    Update grabVersion.js

--- js/grabVersion.js
+++ js/grabVersion.js
@@ -1,19 +1,42 @@
 ssID = '1zJrDzjWoE_n1-K206jGDQe_wyRN804k14F0kQa89NNE'
 
-function get() {
-    var params = {
-    // The ID of the spreadsheet to retrieve data from.
-    spreadsheetId: ssID,
+function initClient() {
+    var API_KEY = 'AIzaSyASNuJPFhwaL5q7Lyks5nvu9ijG2kA_Rto'; // Key
 
-    // The A1 notation of the values to retrieve.
-    range: 'Sheet1',
-    };
+    var CLIENT_ID = '662419024901-tnpj18nn20722gnknooffbcd7cfaqedd.apps.googleusercontent.com'; // Cl ID
+    
+    var SCOPE = 'https://www.googleapis.com/auth/spreadsheets'; // Scope
 
-    var request = gapi.client.sheets.spreadsheets.values.get(params);
-    request.then(function(response) {
-    }, function(reason) {
-    console.error('error: ' + reason.result.error.message);
+    gapi.client.init({
+    'apiKey': API_KEY,
+    'clientId': CLIENT_ID,
+    'scope': SCOPE,
+    'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    }).then(function() {
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
+    updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
     });
+}
+
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+    version();
+}
+
+function updateSignInStatus(isSignedIn) {
+    if (isSignedIn) {
+    version();
+    notify("Signed in.", '#00744d');
+    }
+}
+
+function handleSignInClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+function handleSignOutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+    notify("Signed out.", '#747200');
 }
 
 function version() {
@@ -37,39 +60,3 @@ function version() {
     });
 }
 version();
-
-function initClient() {
-    var API_KEY = 'AIzaSyASNuJPFhwaL5q7Lyks5nvu9ijG2kA_Rto'; // Key
-
-    var CLIENT_ID = '662419024901-tnpj18nn20722gnknooffbcd7cfaqedd.apps.googleusercontent.com'; // Cl ID
-    
-    var SCOPE = 'https://www.googleapis.com/auth/spreadsheets'; // Scope
-
-    gapi.client.init({
-    'apiKey': API_KEY,
-    'clientId': CLIENT_ID,
-    'scope': SCOPE,
-    'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-    }).then(function() {
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
-    updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    });
-}
-
-function handleClientLoad() {
-    gapi.load('client:auth2', initClient);
-}
-
-function updateSignInStatus(isSignedIn) {
-    if (isSignedIn) {
-    get();
-    }
-}
-
-function handleSignInClick(event) {
-    gapi.auth2.getAuthInstance().signIn();
-}
-
-function handleSignOutClick(event) {
-    gapi.auth2.getAuthInstance().signOut();
-}
